f1601fb25134028a5dc10304c4304c5444ea2565
vDK-TFK <113957562+vDK-TFK@users.noreply.github.com>
2023-02-22 19:31:00 -0600

    Initial commit

---
+++ src/main/java/com/Pra03/Pra03Application.java
@@ -0,0 +1,13 @@
+package com.Pra03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pra03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pra03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pra03/Pra03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pra03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pra03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
